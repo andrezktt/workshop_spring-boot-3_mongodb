a178e2582dbcea6a165ee9d0420343bf14f652c3
Andre Zicatti <andrezicatti@gmail.com>
2024-10-24 19:13:58 -0300

    project created

---
+++ src/main/java/com/project/workshop_mongodb/WorkshopSpringBoot3MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.project.workshop_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBoot3MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBoot3MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/workshop_mongodb/WorkshopSpringBoot3MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workshop_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBoot3MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
