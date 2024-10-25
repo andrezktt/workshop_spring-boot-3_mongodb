54ad9bf8a5e4cef38f52f059759f6b84be7ce0ea
Andre Zicatti <andrezicatti@gmail.com>
2024-10-25 15:11:40 -0300

    implementing dto (data object transfer) pattern

--- src/main/java/com/project/workshop_mongodb/resources/UserResources.java
+++ src/main/java/com/project/workshop_mongodb/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.project.workshop_mongodb.resources;
 
 import com.project.workshop_mongodb.domain.User;
+import com.project.workshop_mongodb.dto.UserDTO;
 import com.project.workshop_mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,8 +19,9 @@ public class UserResources {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>>  findAll() {
+    public ResponseEntity<List<UserDTO>>  findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(e -> new UserDTO(e)).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
