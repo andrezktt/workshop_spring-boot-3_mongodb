d4c51766ca4024be88c444d3824e8fd1c0768b4f
Andre Zicatti <andrezicatti@gmail.com>
2024-10-25 14:28:37 -0300

    mongodb connection with repository and services

---
+++ src/main/java/com/project/workshop_mongodb/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.workshop_mongodb.repository;
+
+import com.project.workshop_mongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/project/workshop_mongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.project.workshop_mongodb.services;
+
+import com.project.workshop_mongodb.domain.User;
+import com.project.workshop_mongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
