3043ee09515d09d88f011cb45977d55e659f6695
Andre Zicatti <andrezicatti@gmail.com>
2024-10-28 14:25:20 -0300

    author's data projection using data transfer object pattern

--- src/main/java/com/project/workshop_mongodb/config/Instantiation.java
+++ src/main/java/com/project/workshop_mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.project.workshop_mongodb.config;
 
 import com.project.workshop_mongodb.domain.Post;
 import com.project.workshop_mongodb.domain.User;
+import com.project.workshop_mongodb.dto.AuthorDTO;
 import com.project.workshop_mongodb.repository.PostRepository;
 import com.project.workshop_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -35,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User kdot = new User(null, "Kendrick Lamar", "kdot@gmail.com");
         User billy = new User(null, "Billy Woods", "b.woods@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", mike);
-        Post post2 = new Post(null, sdf.parse("23/08/2018"), "Bom dia", "Acordei feliz hoje!", mike);
-
         userRepository.saveAll(Arrays.asList(mike, kdot, billy));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(mike));
+        Post post2 = new Post(null, sdf.parse("23/08/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(mike));
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
